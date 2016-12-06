35301d4563c18db2bf2fda493ae0159c0dd203d2
Balazs Baltringer <balazs.baltringer@novatec-gmbh.de>
2016-12-06 21:14:46 +0100

    Adapt LocalConfig

--- src/main/java/de/novatec/cg/sc/hanabi/common/configuration/LocalConfig.java
+++ src/main/java/de/novatec/cg/sc/hanabi/common/configuration/LocalConfig.java
@@ -4,7 +4,7 @@ public class LocalConfig implements Config {
 
     private static final String HOST_NAME = "localhost";
     private static final int SERVER_PORT = 4444;
-    private static final String SERVER_URI = "ws://" + HOST_NAME + ":" + SERVER_PORT + "/hanabi";
+    private static final String SERVER_URI = "ws://" + HOST_NAME + ":" + SERVER_PORT;
 
     @Override
     public String getServerURI() {
